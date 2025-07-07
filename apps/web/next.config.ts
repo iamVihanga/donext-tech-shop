2c5170dba5d1306b77a807966828abadc0d49536
iamVihanga <vihangarashansilva@gmail.com>
2025-07-07 16:06:18 +0530

    Fix: nextjs.config.ts rewrite path

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -41,7 +41,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*"
+        destination: "https://api.gamezonetech.lk/api/:path*"
       }
     ];
   }
