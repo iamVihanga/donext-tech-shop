d08b4cb00224c28f28f2637ccaff83d02f8da034
iamVihanga <vihangarashansilva@gmail.com>
2025-07-07 16:18:37 +0530

    Add: env file finetune

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -8,8 +8,8 @@ const envSchema = z.object({
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace"]),
   DATABASE_URL: z.string(),
 
-  CLIENT_APP_URL: z.string().default("http://localhost:3000"),
-  BETTER_AUTH_URL: z.string().default("http://localhost:8000")
+  CLIENT_APP_URL: z.string().default("https://gamezonetech.lk"),
+  BETTER_AUTH_URL: z.string().default("https://api.gamezonetech.lk")
 });
 
 export type EnvSchema = z.infer<typeof envSchema>;
